5a9b29e11a7c32fd3a0794c6af0535aef21ccbb2
Aklima Akter Akhi <aakhi88@gmail.com>
2023-01-22 17:21:03 +0600

    tag model,migration,controller,route,view add

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -56,6 +57,7 @@ Route::group(['prefix' => 'admin', 'middleware' => ['auth']], function(){
     });
 
     Route::resource('category', CategoryController::class);
+    Route::resource('tag', TagController::class);
 
 
 
